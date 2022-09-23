0bfea62cd184bb84336146ccf6a7048f6fa3aea0
nicolofp <nicolo.fp@gmail.com>
2022-09-22 21:13:45 -0400

    Add new code for eigenvalues

--- eigen_svd.jl
+++ eigen_svd.jl
@@ -1,5 +1,4 @@
-using HTTP, JSON, LinearAlgebra, DataFrames, Statistics
-using MLDataUtils: shuffleobs, stratifiedobs, rescale!
+using HTTP, JSON, LinearAlgebra, DataFrames, Statistics, Plots
 
 resp = HTTP.get("https://provanik.s3.us-east-2.amazonaws.com/data_serum.json")
 str = String(resp.body)
@@ -13,4 +12,9 @@ X = X .- mean(X,dims=1)
 Y = (X'*X)./1197
 
 eig_Y = eigen(Y);
-eig_Y.values/sum(eig_Y.values)
+eig_expl = sort(eig_Y.values/sum(eig_Y.values), rev = true)
+p = bar(1:177,eig_expl)
+ylabel!("% var explained")
+xlabel!("Components")
+title!("Eigen serum metabolites")
+savefig(p,"eigen_svd.png")
